7f391107de3f9bfeacfabd1f5a1c34e0c5523603
Frederick Akalin <akalin@keyba.se>
2018-01-04 17:49:52 -0600

    Use channel component

--- shared/common-adapters/markdown.desktop.js
+++ shared/common-adapters/markdown.desktop.js
@@ -1,6 +1,7 @@
 // @flow
 import React, {PureComponent} from 'react'
 import Text from './text'
+import Channel from './channel-container'
 import Mention from './mention-container'
 import Box from './box'
 import Emoji from './emoji'
@@ -69,12 +70,7 @@ function messageCreateComponent(type, key, children, options) {
     case 'mention':
       return <Mention username={children[0]} service={options.service || ''} key={key} style={wrapStyle} />
     case 'channel':
-      // TODO: Render as link.
-      return (
-        <Text type="Body" key={key} style={codeSnippetStyle}>
-          #{children}
-        </Text>
-      )
+      return <Channel channel={children[0]} key={key} style={linkStyle} />
     case 'inline-code':
       return (
         <Text type="Body" key={key} style={codeSnippetStyle}>
